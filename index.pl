587ab6611018cc8f4a763c0daf073dbbca1b079c
kyle steinkamp <kyle@hobopanda.com>
2014-02-20 07:32:36 -0600

    Update index.pl

--- index.pl
+++ index.pl
@@ -1,3 +1,8 @@
+#-----------------------------------------------------------------------#
+# Author: Kyle Steinkamp                                                #
+# Date:   2013/01/01 11:12:13                                           #
+# Email:  kyle@hobopanda.com                                            #
+#-----------------------------------------------------------------------#
 #
 #
 # this is the web version of the transmistion calculator.
